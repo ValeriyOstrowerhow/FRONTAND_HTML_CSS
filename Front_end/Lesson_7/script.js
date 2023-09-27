c076bece94ad87d64fd14d6ea9c4adb3f6ed17f4
Valeriy Ostrowerhow <valeriy22@web.de>
2023-09-27 18:09:14 +0200

    comm

---
+++ Front_end/Lesson_7/script.js
@@ -0,0 +1,19 @@
+console.log('helo, js!');
+let x = 2
+x = 4
+console.log('x==>', x);
+const y = 10
+y = 12
+console.log('y===>', y);
+console.log('распечатается ли текст?');
+
+const btnElement = document.getElementById('btn1');
+console.log('btnElement', btnElement);
+
+let pElementsByTag
+pElemensByTag = document.getElementsByTagName('p');
+console.log('pElementsByTag', pElementsByTag);
+
+const pElementsByClass = document.getElementsByClassName('paragraph');
+console.log('pElementsByClass', pElementsByClass);
+console.log('document', document);
