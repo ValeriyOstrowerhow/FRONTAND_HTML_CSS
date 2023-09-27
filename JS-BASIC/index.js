042e10557c661303760815cde7acb7112b15abe1
Valeriy Ostrowerhow <valeriy22@web.de>
2023-09-27 21:55:07 +0200

    2

--- JS-BASIC/index.js
+++ JS-BASIC/index.js
@@ -1,1 +1,23 @@
-console.log("Hello, world!")
+"use strict"
+const x = 'word'
+    let y = 'another word'
+console.log('y', y);
+
+let str1 = 'это строка'
+let str2 = 'тоже строка'
+let str3 = `$ {str1} и это это строка`
+console.log(str3);
+
+let num1 = 2
+let num2 = 2.2
+console.log(typeof num2);
+
+let boolean1 = true
+let boolean2 = false
+let string = 'что.то другое'
+console.log(typeof boolean1);
+
+let smth
+console.log(smth);
+
+console.log(Number.MAX_SAFE_INTEGER)
