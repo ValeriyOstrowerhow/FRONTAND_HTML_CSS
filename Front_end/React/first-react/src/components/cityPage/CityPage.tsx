3fac89bfd5b85fecdbf67af08e9e69b157095281
Valeriy Ostrowerhow <valeriy22@web.de>
2023-10-23 20:12:17 +0200

    react2

--- Front_end/React/first-react/src/components/cityPage/CityPage.tsx
+++ Front_end/React/first-react/src/components/cityPage/CityPage.tsx
@@ -2,7 +2,8 @@ import { FC } from "react"
 
 const CityPage: FC = () => (
     <div>
-        <h1>CityPage</h1>
+        <h1>Berlin</h1>
+        <p>Capital of Germany</p>
     </div>
 )
 export default CityPage
